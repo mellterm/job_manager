92fb9fe24f6eb04250f358fd576551110c6ace3e
Vince Vincent <mellterm@gmail.com>
2010-12-30 17:58:42 +0000

    housekeeping

--- job_manager/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-JobManager::Application.config.secret_token = 'ba619ee72bc2b37534511053d81494900d607b6fb6f43a6fca8180cd1c5ae66052e5dd810f3dc686f319732ffe81ce5f41f8048c79b98186e1be47e10de913f2'
